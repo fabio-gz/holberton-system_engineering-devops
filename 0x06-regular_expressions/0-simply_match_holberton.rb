a340c49df6ed13da8536abcdce973a3f2dd4221b
fabio-gz <fabiogomez.silva@gmail.com>
2020-02-24 15:31:04 +0000

    [fix]doc typo

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# match 'Holverton'
+# match 'Holberton'
 puts ARGV[0].scan(/Holberton/).join
--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# match 'Holverton'
+# match 'Hbtn'
 puts ARGV[0].scan(/hbt{2,5}n/).join
