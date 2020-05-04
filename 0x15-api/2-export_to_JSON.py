5b19419a23203f3308deac1275d88ebb7ef954b1
fabio-gz <fabiogomez.silva@gmail.com>
2020-05-04 20:44:07 +0000

    [Add]export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""export data in the JSON format"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    todos = requests.get('http://jsonplaceholder.typicode.com/todos',
+                         params={'userId': sys.argv[1]})
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                         format(sys.argv[1]))
+
+    with open(sys.argv[1] + '.json', mode='w') as f:
+        dic = {}
+        lis = []
+        for i in todos.json():
+            dic['task'] = i.get('title')
+            dic['completed'] = i.get('completed')
+            dic['username'] = users.json().get('username')
+            lis.append(dic)
+        stru = {sys.argv[1]: lis}
+
+        json.dump(stru, f)
