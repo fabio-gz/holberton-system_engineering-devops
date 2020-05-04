8503704278ac14d5b6ab618dd995f88449409918
fabio-gz <fabiogomez.silva@gmail.com>
2020-05-04 17:30:28 +0000

    [Add]point zero

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""for a given employee ID, returns information about his/her TODO list
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    todos = requests.get('http://jsonplaceholder.typicode.com/todos',
+                         params={'userId': sys.argv[1]})
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                         format(sys.argv[1]))
+
+    tasks = 0
+    done = 0
+    l_task = []
+
+    for i in todos.json():
+        tasks += 1
+        if i.get('completed') is True:
+            done += 1
+            l_task.append(i)
+
+    user_name = users.json().get('name')
+    print('Employee {} is done with task({}/{}):'.format(user_name,
+                                                         done, tasks))
+    for j in l_task:
+        print('\t', j.get('title'))
